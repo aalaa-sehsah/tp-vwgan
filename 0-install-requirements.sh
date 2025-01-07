749b9325e78f1977f32d4f07fa6c65049d362dc7
Aalaa Sehsah <aalaasehsah@gmail.com>
2025-01-07 23:39:55 +0200

    Update install requirements

--- 0-install-requirements.sh
+++ 0-install-requirements.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-pip install -r pdb-dataset/requirements.txt
-pip install -r TP-VWGAN/requirements.txt
-pip install torch torchvision --index-url https://download.pytorch.org/whl/torch_stable.html
+python3 -m pip install -r pdb-dataset/requirements.txt
+python3 -m pip install -r TP-VWGAN/requirements.txt
+python3 -m pip install torch torchvision --index-url https://download.pytorch.org/whl/torch_stable.html
 
 read -rsp 'Press <Enter> to exit...'
